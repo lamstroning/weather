3a1f67e4e504eadb069bda5252bf22e8dd44adf7
lamstroning <lexashob@yandex.ru>
2021-10-17 21:55:43 +0300

    add images

--- src/components/Home/FutureWeather.tsx
+++ src/components/Home/FutureWeather.tsx
@@ -19,10 +19,18 @@ export class FutureWeather extends React.Component <any, State> {
 			.then(response => response.json())
 			.catch(console.error)
 			.then(({forecast}) => {
-				forecast.forecastday.forEach((forecast: {date: string; date_epoch: number;}) =>
-					forecast.date = getDate(forecast.date_epoch * 1000))
+				const forecastData:any = [];
+				forecast.forecastday.forEach((forecast: any) =>
+				{
+					const forecastItem:any = {}
+					forecastItem.date = getDate(forecast.date_epoch * 1000);
+					forecastItem.icon = forecast.day.condition.icon.replace('//cdn.weatherapi.com', '/images');
+					forecastItem.maxTemp = Math.round(forecast.day.maxtemp_c);
+					forecastItem.minTemp = Math.round(forecast.day.mintemp_c);
+					forecastData.push(forecastItem);
+				})
 				this.setState({
-					forecast: forecast.forecastday
+					forecast: forecastData
 				})
 				console.log(forecast);
 			})
@@ -34,6 +42,19 @@ export class FutureWeather extends React.Component <any, State> {
 				<div className='row weather'>
 					{this.state.forecast.map((forecast, index) =>
 						<div className='col weather__card' key={forecast.date}>
+							<img src={forecast.icon} alt=''/>
+							<div className='container'>
+								<div className='row'>
+									<div className='col'>
+										{forecast.maxTemp > 0 ? `+${forecast.maxTemp}` : forecast.maxTemp}
+									</div>
+								</div>
+								<div className='row'>
+									<div className='col'>
+										{forecast.minTemp > 0 ? `+${forecast.minTemp}` : forecast.minTemp}
+									</div>
+								</div>
+							</div>
 							<div className='weather__date'>
 								{forecast.date}
 							</div>
